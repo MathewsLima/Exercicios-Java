b2eac69c423ad0e25806fd0bf78a38852f1c7c55
Mathews Lima <contatomathewslima@gmail.com>
2023-06-14 23:59:13 -0300

    exercicios

--- Exercicio3/app/src/main/java/Exercicio3/App.java
+++ Exercicio3/app/src/main/java/Exercicio3/App.java
@@ -1,9 +1,22 @@
+/*
+Escreva um algoritmo para determinar o consumo médio de um automóvel
+sendo fornecida a distância total percorrida pelo automóvel e o
+total de combustível gasto;
+*/
+
 package Exercicio3;
+
+import java.util.Scanner;
 
 public class App {
    
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe a distancia percorrida: ");
+        float 
+        
     }
 }
