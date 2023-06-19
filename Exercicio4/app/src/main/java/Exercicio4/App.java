6b954339e589b70b5176cfa0e12bcf137b3b219f
Mathews Lima <contatomathewslima@gmail.com>
2023-06-19 00:00:34 -0300

    exercicios

--- Exercicio4/app/src/main/java/Exercicio4/App.java
+++ Exercicio4/app/src/main/java/Exercicio4/App.java
@@ -14,6 +14,7 @@ public class App {
         
         Scanner leitor = new Scanner(System.in);
         
+        //Entrada de Dados.
         System.out.println("Digite o nome do vendedor:");
         String nomeVen = leitor.next();
         System.out.println("Digite o sálario base:");
@@ -21,9 +22,11 @@ public class App {
         System.out.println("Digite o total de vendas:");
         int vendas = leitor.nextInt();
         
+        //Calculo dos dados.
         float comissao = (15 * vendas) / 100;
         float salarioFinal = sBase + comissao;
         
+        //Saída dos dados.
         System.out.println("O vendedor " + nomeVen + " com o salário base"
                 + " de R$ " + sBase + " com base na quantidade de vendas"
                         + " esse mês o salário final será de R$" + salarioFinal);
