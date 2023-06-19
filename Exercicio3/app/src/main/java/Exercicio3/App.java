acb84dc9c94f074fafc87203823065736b580afc
Mathews Lima <contatomathewslima@gmail.com>
2023-06-18 22:41:53 -0300

    exercicio

--- Exercicio3/app/src/main/java/Exercicio3/App.java
+++ Exercicio3/app/src/main/java/Exercicio3/App.java
@@ -16,7 +16,15 @@ public class App {
         Scanner leitor = new Scanner(System.in);
 
         System.out.println("Informe a distancia percorrida: ");
-        float 
+        float kmPercorrido = leitor.nextFloat();
+        System.out.println("Quanto combustível foi gasto? ");
+        float valorComb = leitor.nextFloat();
+        
+        float media = kmPercorrido / valorComb;
+        
+        
+        System.out.println("A média de consumo foi de: " + media + " km/l");
+               
         
     }
 }
