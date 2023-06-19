233824aecf9e6b6aa5a37f5d46c042da55ec75bc
Mathews Lima <contatomathewslima@gmail.com>
2023-06-19 00:54:12 -0300

    exercicios

---
+++ exercicio8/app/src/main/java/exercicio8/App.java
@@ -0,0 +1,24 @@
+/*
+ * Elabore um algoritmo que efetue a apresentação do valor da conversão em real (R$) de um 
+ * valor lido em dólar (US$). O algoritmo deverá solicitar o valor da cotação do dólar 
+ * e também a quantidade de dólares disponíveis com o usuário;
+ */
+package exercicio8;
+import java.util.Scanner;
+public class App {
+    
+    public static void main(String[] args) {
+    Scanner leitoScanner = new Scanner(System.in);
+    
+    System.out.println("Digite quantos reais você possue: ");
+    float real = leitoScanner.nextFloat();
+    System.out.println("Digite a cotação do dólar:");
+    float dolar = leitoScanner.nextFloat();
+
+    float cotacao = real / dolar;
+
+    System.out.println("De acordo com a cotação informada você possu USD $" + cotacao);
+
+
+    }
+}
